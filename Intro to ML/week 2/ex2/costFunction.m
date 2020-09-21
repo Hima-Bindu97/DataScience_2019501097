9b7f8637898bd477c3b41ebcef5df26b0e885200
Hima-Bindu97 <himabindureddygotike@msitprogram.net>
2020-09-21 21:15:49 +0530

    week2

--- Intro to ML/week 2/ex2/costFunction.m
+++ Intro to ML/week 2/ex2/costFunction.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -21,7 +21,9 @@ grad = zeros(size(theta));
 %
 
 
-
+h_theta = sigmoid(X*theta);
+J = (1 / m) * ((-y' * log(h_theta)) - (1 - y)' * log(1 - h_theta));
+grad = (1 / m) * (h_theta - y)' * X;
 
 
 
