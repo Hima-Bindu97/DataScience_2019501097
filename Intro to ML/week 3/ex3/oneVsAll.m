37bf2ef4fae5ba190c6d880b8f5d3baeb5ae6afb
Hima-Bindu97 <himabindureddygotike@msitprogram.net>
2020-10-06 22:15:45 +0530

    done

--- Intro to ML/week 3/ex3/oneVsAll.m
+++ Intro to ML/week 3/ex3/oneVsAll.m
@@ -57,7 +57,9 @@ X = [ones(m, 1) X];
 
 
 for c = 1:num_labels 
-        all_theta(c, :) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+ initial_theta = zeros(n + 1, 1);
+ options = optimset('GradObj', 'on', 'MaxIter', 50);
+  all_theta(c, :) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
 
 
 
